bd6b451c0d1fe24ba7733bab5c9bd5585d792ff7
davidlechuga <david.lechuga@lakmisystems.com.mx>
2020-04-17 00:27:16 -0500

    server.js not mongo uri

--- LocalHackDay_Raccoons2019-master/vue-Login-master/server.js
+++ LocalHackDay_Raccoons2019-master/vue-Login-master/server.js
@@ -1,26 +1,26 @@
-var express = require ("express");
+var express = require('express');
 
-var cors = require ("cors");
-var bodyParser = require ("body-parser");
-var app = express ();
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var app = express();
 
-var mongoose = require ("mongoose");
-var port = process.env.PORT || 5000
+var mongoose = require('mongoose');
+var port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const mongoURI = 'mongodb+srv://Hacks:david2001@cluster0-arnoh.mongodb.net/test'
+const mongoURI = '';
 
-mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
-    .then ( () => console.log("MongoDB Connected"))
-    .catch (err => console.log(err));
+mongoose
+	.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+	.then(() => console.log('MongoDB Connected'))
+	.catch((err) => console.log(err));
 
-var Users = require ("./routes/Users")
+var Users = require('./routes/Users');
 
-app.use("/users", Users)
-app.listen(port, function () {
-    console.log("SERVER IS RUNNING ON PORT " + port );
-    
-})
+app.use('/users', Users);
+app.listen(port, function() {
+	console.log('SERVER IS RUNNING ON PORT ' + port);
+});
